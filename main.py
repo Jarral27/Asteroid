40b0d9f9e2f3e5e9d6b76cd1cc354273d7280b2b
Jarral27 <jarral2710@gmail.com>
2025-08-02 19:04:29 +0200

    completed players code

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)  # Set the container for Player class
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))  # Fill the screen with black
-        pygame.display.flip()  # Update the display
+        
+        updatable.update(dt)
+        
+
+        #always update before rendering
+        
+        screen.fill("black")  # Fill the screen with black
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        dt = clock.tick(60) / 1000.0  # Limit to 60 FPS
+        
+        
 
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+        pygame.display.flip()  # Update the display !alwways last!
 
 if __name__ == "__main__":
     main()
